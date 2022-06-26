34ac3db605db89e685a989917cbc435c17a85364
Abmael <abmael.cont@gmail.com>
2022-06-26 09:31:39 -0300

    Check if a given object is valid for a POST request

--- src/domain/create.js
+++ src/domain/create.js
@@ -3,3 +3,24 @@ class NotValidObjectException extends Error {
         super('Not Valid Object Exception');
     }
 }
+
+class ObjectValidator {
+    validate(item) {
+        const allowedProperties = ['title', 'tags', 'link', 'description'];
+        
+        let hasProperty;
+        let validation = true;
+
+        for(let prop of allowedProperties) {
+            hasProperty = Object.prototype.hasOwnProperty.call(item, prop);
+            
+            if(!hasProperty) {
+                validation = false;
+                
+                break;
+            }
+        }
+
+        return validation;
+    }
+}
