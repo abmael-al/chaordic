0965713042febab04f359e89a4c57e1e4edfec4d
Abmael <abmael.cont@gmail.com>
2022-06-26 09:35:23 -0300

    Handle request to search a tool by name or tag

--- src/domain/search.js
+++ src/domain/search.js
@@ -1,5 +1,67 @@
+import { fetch } from "../scripts/util";
+
 class NotAllowedTypeOfSearchException extends Error {
     constructor() {
         super('Not Allowed Type of Search Exception');
     }
+}
+
+export class Search {
+    async search(request, type) {
+        let isAllowedType = this.#validate(type);
+
+        if(!isAllowedType) {
+            throw new NotAllowedTypeOfSearchException();
+        }
+
+        const params = this.#formatParams(request, type);
+        const url = this.#buildUrl(params, type);
+
+        return await fetch(url);
+    }
+    
+    #validate(type) {
+        let validation;
+
+        switch(type) {
+            case "name":
+                validation = true;
+                break;
+
+            case "tag":
+                validation = true;
+                break;
+    
+            default:
+                validation = false;
+                break;
+        }
+
+        return validation;
+    }
+
+    #buildUrl(params, type) {
+        const possibleUrls = {
+            "name":`http://localhost:3000/tools?q=${params}`,
+            "tag": `http://localhost:3000/tools?tags_like=${params}`,
+        }
+
+        return possibleUrls[type];
+    }
+
+    #formatParams(request, type) {
+        const handlers = {
+            "name": (name) => {
+                return name;
+            },
+            "tag": (tags) => {
+                return tags.split(' ').join('&');
+            }
+        }
+
+        const formatHandler = handlers[type];
+
+        return formatHandler(request);
+    }
+
 }
