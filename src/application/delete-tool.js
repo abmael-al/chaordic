0062c524e7c45007e7bbb9e54c142cca141a79da
Abmael <abmael.cont@gmail.com>
2022-06-27 17:06:03 -0300

    Listen to user request to delete a tool and call handler

--- src/application/delete-tool.js
+++ src/application/delete-tool.js
@@ -1,3 +1,52 @@
+import { DeleteUtilityTool } from '../domain/delete';
+
+export class UtilityToolRemovalListener {  
+    #confirmationModal = document.querySelector('[data-remove-tool-confirmation-modal]');
+    #targetTool;
+    #handlerCallback;
+
+    constructor(root) {
+        this.root = root;
+        this.#setConfirmationModal();
+    }
+
+    listen(callback) {
+        this.root.addEventListener('click', async (event) => {
+            const { target } = event;
+            const element = target.parentElement;
+
+            event.preventDefault();
+            
+            if(element.getAttribute('data-utility-tool-element') === 'button') {
+                this.#targetTool = element;
+                this.#handlerCallback = callback;
+
+                this.#confirmationModal.showModal();
+            }
+        })
+    }
+
+    #setConfirmationModal() {
+        this.#confirmationModal.addEventListener('click', (event) => {
+            const { target } = event;
+
+            if(target.hasAttribute('data-cancel')) {
+                this.#confirmationModal.close();
+            }
+            else if(target.hasAttribute('data-confirm')) {
+                new UtilityToolRemovalHandler(this.#targetTool).handle(this.#handlerCallback);
+            
+                this.#confirmationModal.close();
+            }
+        });
+
+        this.#confirmationModal.addEventListener('close', () => {
+            this.#targetTool = null;
+            this.#handlerCallback = undefined;
+        })
+    }
+}
+
 class UtilityToolRemovalHandler {
     constructor(target) {
         this.target = target;
