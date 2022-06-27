3c652fcd5a5eccf7b3b45a7ab2e4b21b568bfc0f
Abmael <abmael.cont@gmail.com>
2022-06-27 16:54:25 -0300

    Format all input fields values

--- src/application/submit-tool.js
+++ src/application/submit-tool.js
@@ -1,3 +1,26 @@
+class InputFieldsValuesFormater {
+    format(inputFields) {
+        const fields = Array.from(inputFields);
+                
+        const values = fields.reduce((acc, input) => {   
+                const key = input.id;
+                const value = this.#getValue(key, input);
+    
+                return ({
+                    ...acc,
+                    [key] : value
+                });
+            }, {}
+        );
+
+        return values;
+    }
+
+    #getValue(key, input) {
+        return (key !== 'tags') ? input.value : input.value.split(' ');
+    }
+}
+
 class InputFieldsCleaner {
     async clean(inputFields) {
         inputFields.forEach((field) => {
